ef134ba9834745f92e910a8c4e7be1b57dd61f5b
Diesel127 <retina127127@gmail.com>
2026-03-22 17:57:17 +0100

    add Dockerfile

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,15 +1,18 @@
-const allImgBloks = document.querySelectorAll('.hero__img');
-const randomIndex = Math.floor(Math.random() * allImgBloks.length);
-const randomBlock = allImgBloks[randomIndex];
-randomBlock.classList.add('is-visible');
-
-document.body.style.setProperty('background-color', '#151515');
-
 document.addEventListener('DOMContentLoaded', function () {
+    const allImgBlocks = document.querySelectorAll('.hero__img');
+    if (allImgBlocks.length > 0) {
+        const randomIndex = Math.floor(Math.random() * allImgBlocks.length);
+        allImgBlocks[randomIndex].classList.add('is-visible');
+    }
+
+    if (window.location.pathname === '/') {
+        document.body.style.setProperty('background-color', '#151515');
+    }
+
     const showcaseButton = document.querySelector('.header__button-btn');
     if (showcaseButton) {
         showcaseButton.addEventListener('click', function () {
-            window.location.href = './form/upload.html';
+            window.location.href = '/upload';
         });
     }
 });
