dd136255f43f9905a1f9daed8490abeebe058299
Diesel127 <retina127127@gmail.com>
2026-03-23 15:51:02 +0100

    css fix

---
+++ backups/backup_2026-03-10_211820.sql
@@ -0,0 +1,101 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict qZGrPORAcmAAtlzhHI5R8SjlEgWIbJVahrTCAtYnz5X9B8lrWiUviFK9Q6WIlP8
+
+-- Dumped from database version 15.17 (Debian 15.17-1.pgdg13+1)
+-- Dumped by pg_dump version 15.17 (Debian 15.17-1.pgdg13+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: images; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.images (
+    id integer NOT NULL,
+    filename text NOT NULL,
+    original_name text NOT NULL,
+    size integer NOT NULL,
+    file_type text NOT NULL,
+    upload_time timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.images OWNER TO postgres;
+
+--
+-- Name: images_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.images_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.images_id_seq OWNER TO postgres;
+
+--
+-- Name: images_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.images_id_seq OWNED BY public.images.id;
+
+
+--
+-- Name: images id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.images ALTER COLUMN id SET DEFAULT nextval('public.images_id_seq'::regclass);
+
+
+--
+-- Data for Name: images; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.images (id, filename, original_name, size, file_type, upload_time) FROM stdin;
+4	759b792c-a44f-4896-97a6-8759bef4b182.jpg	test_img3.jpg	100213	jpg	2026-03-10 13:16:54.222787
+5	3425b68c-8d89-45c8-be26-79b0eb3c3bda.jpg	test_img5.jpg	98888	jpg	2026-03-10 13:16:56.755337
+6	0cbfcff6-2587-4bfe-9d7c-3f0e89ec2bde.jpg	test_img6.jpg	76410	jpg	2026-03-10 13:16:59.210222
+\.
+
+
+--
+-- Name: images_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.images_id_seq', 6, true);
+
+
+--
+-- Name: images images_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.images
+    ADD CONSTRAINT images_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict qZGrPORAcmAAtlzhHI5R8SjlEgWIbJVahrTCAtYnz5X9B8lrWiUviFK9Q6WIlP8
+
